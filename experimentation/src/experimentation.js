bfcb146ae5e0044bfa260e0f053a778d57f80ac3
foxdonut <fdaoud@fastmail.com>
2016-12-25 13:52:06 -0500

    Watch me WIP

--- experimentation/src/experimentation.js
+++ experimentation/src/experimentation.js
@@ -19,7 +19,6 @@ const meiosis = initialModel => {
       objectPath.set(state, path, component.state(objectPath.get(model, path), objectPath.get(state, path)));
       return state;
     }),
-    view: component.view && (model => component.view(objectPath.get(model, path))),
     initialModel: component.initialModel
   });
 
@@ -67,19 +66,6 @@ const pipeIn = function() {
 
 const counterComponent = (propose, id) => {
   const remove = !!id;
-  id = id || "counter_" + String(new Date().getTime());
-
-  const events = propose => ({
-    onIncrease: _evt => propose({ counterId: id, add:  1 }),
-    onDecrease: _evt => propose({ counterId: id, add: -1 }),
-    onRemove: _evt => propose({ counterId: id, removeCounter: true })
-  });
-
-  const createView = events => model => m("div",
-    m("span", "Counter: " + model.counter + " " + (model.even ? "Even" : "Odd")),
-    m("button.btn.btn-sm.btn-primary", { onclick: events.onIncrease }, "Increase"),
-    m("button.btn.btn-sm.btn-default", { onclick: events.onDecrease }, "Decrease"),
-    remove ? m("button.btn.btn-sm.btn-danger", { onclick: events.onRemove }, "Remove") : null);
 
   const initialModel = { counter: 0 };
 
@@ -89,8 +75,6 @@ const counterComponent = (propose, id) => {
     }
     return model;
   };
-
-  const view = pipeIn(propose, events, createView);
 
   const state = (model, state) => {
     state.even = model.counter % 2 === 0;
@@ -103,7 +87,21 @@ const counterComponent = (propose, id) => {
     }
   };
 
-  return { initialModel, receive, view, state, nextAction };
+  return { initialModel, receive, state, nextAction };
+};
+
+const counterView = (propose, id, remove, model) => {
+  const events = ({
+    onIncrease: _evt => propose({ counterId: id, add:  1 }),
+    onDecrease: _evt => propose({ counterId: id, add: -1 }),
+    onRemove: _evt => propose({ counterId: id, removeCounter: true })
+  });
+
+  return m("div",
+    m("span", "Counter: " + model.counter + " " + (model.even ? "Even" : "Odd")),
+    m("button.btn.btn-sm.btn-primary", { onclick: events.onIncrease }, "Increase"),
+    m("button.btn.btn-sm.btn-default", { onclick: events.onDecrease }, "Decrease"),
+    remove ? m("button.btn.btn-sm.btn-danger", { onclick: events.onRemove }, "Remove") : null);
 };
 
 const initialModel = { counter: 0, counterIds: [], countersById: {} };
@@ -111,16 +109,17 @@ const { propose, components, stateFn, state, nestComponent } = meiosis(initialMo
 
 const componentsById = {};
 
-const topCounter = counterComponent(propose);
+const id = "counter_" + String(new Date().getTime());
+const topCounter = counterComponent(propose, id);
 
 const events = propose => ({
   onAddCounter: _evt => propose({ addCounter: true })
 });
 
 const createView = events => model => m("div",
-  topCounter.view(model),
+  counterView(propose, id, false, model),
   m("button.btn.btn-primary", { onclick: events.onAddCounter }, "Add Counter"),
-  model.counterIds.map(id => componentsById[id]).map(component => component.view(model)));
+  model.counterIds.map(id => counterView(propose, id, true, model)));
 
 const view = pipeIn(propose, events, createView);
 
