633fcf356dabbcebd458b8ff91215f8ce533c36f
foxdonut <fdaoud@fastmail.com>
2017-02-12 17:24:38 -0500

    Externalize stream library.

--- lib/meiosis.d.ts
+++ lib/meiosis.d.ts
@@ -1,37 +1,23 @@
-/// <reference path="flyd.d.ts" />
-export declare type Stream<T> = Flyd.Stream<T>;
-export declare type Scanner<A, B> = Flyd.Scanner<A, B>;
-export declare type Mapper<A, B> = Flyd.Mapper<A, B>;
-export interface MapperSpec<A, B> {
-    [name: string]: Mapper<A, B>;
+export interface Mapper<A, B> {
+    (value: A): B;
 }
-export interface ModelChange<M> {
-    (model: M): M;
+export interface Stream<T> {
+    (): T;
+    (value: T): Stream<T>;
+    map<T, R>(mapper: Mapper<T, R>): Stream<R>;
 }
-export interface NextAction {
-    (model: any): void;
+export interface StreamLibrary {
+    stream<T>(): Stream<T>;
+    stream<T>(value: T): Stream<T>;
+    combine<T>(combinator: (...streams: Array<Stream<any>>) => T, streams: Array<Stream<any>>): Stream<T>;
+}
+export interface Scanner<A, B> {
+    (acc: A, next: B): A;
 }
 export interface RunParameters<M> {
-    initialModel: M;
-    modelChanges: Stream<ModelChange<M>>;
-    mappers?: Array<MapperSpec<any, any> | Mapper<any, any>>;
-    nextAction?: NextAction;
+    streams: Array<Stream<any>>;
+    streamLibrary: StreamLibrary;
     copy?: any;
 }
-export interface MeiosisRun<M> {
-    (params: RunParameters<M>): MeiosisApp;
-}
-export interface MeiosisInstance<M> {
-    run: MeiosisRun<M>;
-}
-export interface MeiosisApp {
-    [key: string]: Stream<any>;
-}
-export declare const combine: <A, B, C>(combinator: (stream1: Flyd.Stream<A>, stream2: Flyd.Stream<B>) => C, streams: Flyd.Stream<any>[]) => Flyd.Stream<C>, map: <T, R>(mapper: Flyd.Mapper<T, R>, stream: Flyd.Stream<T>) => Flyd.Stream<R>, merge: <T>(stream1: Flyd.Stream<T>, stream2: Flyd.Stream<T>) => Flyd.Stream<T>, on: <T, R>(mapper: Flyd.Mapper<T, R>, stream: Flyd.Stream<T>) => Flyd.Stream<R>, scan: <A, B>(scanner: Flyd.Scanner<A, B>, initial: A, stream: Flyd.Stream<B>) => Flyd.Stream<A>, stream: {
-    <T>(): Flyd.Stream<T>;
-    <T>(value: T): Flyd.Stream<T>;
-};
-export declare const mergeAll: (streams: Flyd.Stream<any>[]) => Flyd.Stream<{}>;
-declare function newInstance<M>(): MeiosisInstance<M>;
-declare const run: MeiosisRun<any>;
-export { newInstance, run };
+export declare const createMergeAll: (streamLibrary: StreamLibrary) => (streams: Stream<any>[]) => Stream<{}>;
+export declare const createScan: (lib: StreamLibrary) => <A, B>(fn: Scanner<A, B>, acc: A, s: Stream<B>) => Stream<A>;
--- src/meiosis.ts
+++ src/meiosis.ts
@@ -1,175 +1,137 @@
-/// <reference path="../lib/flyd.d.ts" />
-
-import * as flyd from "flyd";
-
-export type Stream<T> = Flyd.Stream<T>;
-export type Scanner<A, B> = Flyd.Scanner<A, B>;
-export type Mapper<A, B> = Flyd.Mapper<A, B>;
-
-export interface MapperSpec<A, B> {
-  [name: string]: Mapper<A, B>;
+export interface Mapper<A, B> {
+  (value: A): B;
 }
 
-export interface ModelChange<M> {
-  (model: M): M;
+export interface Stream<T> {
+  /**
+   * Gets the current value of the stream.
+   */
+  (): T;
+
+  /**
+   * Sets a new value on the stream.
+   */
+  (value: T): Stream<T>;
+
+  /**
+   * Maps over a stream.
+   */
+  map<T, R>(mapper: Mapper<T, R>): Stream<R>;
 }
 
-export interface NextAction {
-  (model: any): void;
+/**
+ * Top-level stream library declarations.
+ */
+export interface StreamLibrary {
+  /**
+   * Creates a stream.
+   */
+  stream<T>(): Stream<T>;
+
+  /**
+   * Creates a stream with an initial value.
+   */
+  stream<T>(value: T): Stream<T>;
+
+  /**
+   * Combines streams.
+   */
+  combine<T>(combinator: (...streams: Array<Stream<any>>) => T,
+    streams: Array<Stream<any>>): Stream<T>;
+}
+  /**
+   * Maps over a stream.
+   */
+  //map<T, R>(mapper: Mapper<T, R>, stream: Stream<T>): Stream<R>;
+
+  /**
+   * Merges two streams.
+   */
+  //merge<T>(stream1: Stream<T>, stream2: Stream<T>): Stream<T>;
+
+  /**
+   * Scans over a stream.
+   */
+  //scan<A, B>(scanner: Scanner<A, B>, initial: A, stream: Stream<B>): Stream<A>;
+
+  /**
+   * Similar to map, but for doing side effects and returns an empty stream.
+   */
+  //on<T, R>(mapper: Mapper<T, R>, stream: Stream<T>): Stream<R>;
+
+export interface Scanner<A, B> {
+  (acc: A, next: B): A;
 }
 
 export interface RunParameters<M> {
-  initialModel: M;
-  modelChanges: Stream<ModelChange<M>>;
-  mappers?: Array<MapperSpec<any, any> | Mapper<any, any>>;
-  nextAction?: NextAction;
+  streams: Array<Stream<any>>;
+  streamLibrary: StreamLibrary;
   copy?: any;//FIXME
 }
 
-export interface MeiosisRun<M> {
-  (params: RunParameters<M>): MeiosisApp;
-}
-
-export interface MeiosisInstance<M> {
-  run: MeiosisRun<M>;
-}
-
-export interface MeiosisApp {
-  [key: string]: Stream<any>;
-}
-
-interface NamedStream {
-  name: string;
-  stream: Stream<any>;
-}
-
-interface NamedValue {
-  name: string;
-  value: any;
-}
-
-export const {
-  combine,
-  map,
-  merge,
-  on,
-  scan,
-  stream
-} = flyd;
-
-export const mergeAll = (streams: Array<Stream<any>>) => {
-  const merged = stream();
-  streams.forEach(s => map(merged, s));
+export const createMergeAll = (streamLibrary: StreamLibrary) => (streams: Array<Stream<any>>) => {
+  const merged = streamLibrary.stream();
+  streams.forEach(s => s.map(merged));
   return merged;
 };
 
-const getName = (value: any) => typeof value === "function" ? undefined : Object.keys(value)[0];
-const getFn = (value: any) => {
-  const name = getName(value);
-  return name ? value[name] : value;
+export const createScan = (lib: StreamLibrary) => function<A, B>(fn: Scanner<A, B>, acc: A, s: Stream<B>) {
+  const result = lib.combine(s => {
+    acc = fn(acc, s());
+    return acc;
+  }, [s]);
+
+  if (!result()) {
+    result(acc);
+  }
+  return result;
 };
 
-function newInstance<M>(): MeiosisInstance<M> {
-  const run = (params: RunParameters<M>): MeiosisApp => {
-    if (!params.initialModel || !params.modelChanges) {
-      throw new Error("Please specify initialModel and modelChanges.");
-    }
-    const streams: MeiosisApp = {};
-    const allStreams: Array<NamedStream> = [];
+/*
+export function trace<M>(params: RunParameters<M>): void {
+  if (!params.streams) {
+    throw new Error("Please specify streams.");
+  }
 
-    let lastStream: Stream<M> = scan((model: M, modelChange: ModelChange<M>) => modelChange(model),
-      params.initialModel, params.modelChanges);
+  const devtool: boolean = window && window["__MEIOSIS_TRACER_GLOBAL_HOOK__"];
+  if (devtool) {
+    const copy: any = params.copy || ((model: M) => JSON.parse(JSON.stringify(model)));
+    const bufferedValues: Array<any> = [];
+    let devtoolInitialized: boolean = false;
+    const sendValues: Stream<boolean> = params.streamLibrary.stream(true);
 
-    const scannerStream = lastStream;
-    const scannerName = "modelChanges";
-    streams[scannerName] = lastStream;
-    allStreams.push({ name: scannerName, stream: lastStream });
+    let changes: Stream<Date> = params.streamLibrary.stream(new Date());
+    let lastChange: Date = changes();
+    on(() => changes(new Date()), params.modelChanges);
 
-    (params.mappers || []).forEach(mapper => {
-      const mapperName: string = getName(mapper);
-      const mapperFn: Mapper<any, any> = getFn(mapper);
-
-      lastStream = map(mapperFn, lastStream);
-      mapperName && (streams[mapperName] = lastStream);
-      allStreams.push({ name: (mapperName || ""), stream: lastStream });
+    window.addEventListener("message", evt => {
+      if (evt.data.type === "MEIOSIS_RENDER_MODEL") {
+        sendValues(evt.data.sendValuesBack);
+        scannerStream(evt.data.model);
+      }
+      else if (evt.data.type === "MEIOSIS_TRACER_INIT") {
+        devtoolInitialized = true;
+        bufferedValues.forEach(values => window.postMessage({ type: "MEIOSIS_VALUES", values }, "*"));
+      }
     });
 
-    const render: Stream<any> = stream();
+    on(() => {
+      const change: Date = changes();
+      const update: boolean = change !== lastChange;
+      lastChange = change;
 
-    /*
-    if (params.nextAction) {
-      let lastProposal: P = propose();
+      const values: Array<NamedValue> = streams.map((namedStream: NamedStream) =>
+        ({ name: namedStream.name, value: copy(namedStream.stream()) }));
 
-      on((value: any) => {
-        const proposal: P = propose();
-
-        render(value);
-        if (proposal !== lastProposal) {
-          params.nextAction(value, proposal);
+      if (sendValues()) {
+        if (devtoolInitialized) {
+          window.postMessage({ type: "MEIOSIS_VALUES", values, update }, "*");
         }
-        lastProposal = proposal;
-      }, lastStream);
-    }
-    else {
-    */
-      on(render, lastStream);
-    //}
-
-    streams["render"] = render;
-
-    const devtool: boolean = window && window["__MEIOSIS_TRACER_GLOBAL_HOOK__"];
-    if (devtool) {
-      const copy: any = params.copy || ((model: M) => JSON.parse(JSON.stringify(model)));
-      const bufferedValues: Array<any> = [];
-      let devtoolInitialized: boolean = false;
-      const sendValues: Stream<boolean> = stream(true);
-
-      let changes: Stream<Date> = stream(new Date());
-      let lastChange: Date = changes();
-      on(() => changes(new Date()), params.modelChanges);
-
-      window.addEventListener("message", evt => {
-        if (evt.data.type === "MEIOSIS_RENDER_MODEL") {
-          sendValues(evt.data.sendValuesBack);
-          scannerStream(evt.data.model);
+        else {
+          bufferedValues.push(values);
         }
-        else if (evt.data.type === "MEIOSIS_TRACER_INIT") {
-          devtoolInitialized = true;
-          bufferedValues.forEach(values => window.postMessage({ type: "MEIOSIS_VALUES", values }, "*"));
-        }
-      });
-
-      on(() => {
-        const change: Date = changes();
-        const update: boolean = change !== lastChange;
-        lastChange = change;
-
-        const values: Array<NamedValue> = allStreams.map((namedStream: NamedStream) =>
-          ({ name: namedStream.name, value: copy(namedStream.stream()) }));
-
-        if (sendValues()) {
-          if (devtoolInitialized) {
-            window.postMessage({ type: "MEIOSIS_VALUES", values, update }, "*");
-          }
-          else {
-            bufferedValues.push(values);
-          }
-        }
-      }, lastStream);
-    }
-
-    return streams;
-  };
-
-  return {
-    run
-  };
-}
-
-const instance = newInstance<any>();
-const run = instance.run;
-
-export {
-  newInstance,
-  run
+      }
+    }, lastStream);
+  }
 };
+*/
