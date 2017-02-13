e08de3fa306378456f9b8a9171619cc0bf0af24b
foxdonut <fdaoud@fastmail.com>
2017-02-13 11:47:18 -0500

    Update WIP.

--- builds/meiosis/v0.10.0a/meiosis.js
+++ builds/meiosis/v0.10.0a/meiosis.js
@@ -82,7 +82,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 "use strict";
 
-exports.createMergeAll = function (streamLibrary) { return function (streams) {
+exports.createMergeIntoOne = function (streamLibrary) { return function (streams) {
     var merged = streamLibrary.stream();
     streams.forEach(function (s) { return s.map(merged); });
     return merged;
@@ -97,6 +97,51 @@ exports.createScan = function (lib) { return function (fn, acc, s) {
     }
     return result;
 }; };
+function trace(params) {
+    if (!params.streamLibrary || !params.modelChanges || !params.streams) {
+        throw new Error("Please specify streamLibrary, modelChanges, and streams.");
+    }
+    var devtool = window && window["__MEIOSIS_TRACER_GLOBAL_HOOK__"];
+    if (devtool) {
+        var copy_1 = params.copy || (function (model) { return JSON.parse(JSON.stringify(model)); });
+        var bufferedValues_1 = [];
+        var devtoolInitialized_1 = false;
+        var sendValues_1 = params.streamLibrary.stream(true);
+        var changes_1 = params.streamLibrary.stream(new Date());
+        var lastChange_1 = changes_1();
+        params.modelChanges.map(function () { return changes_1(new Date()); });
+        var firstStream = params.streams[0];
+        var lastStream = params.streams[params.streams.length - 1];
+        window.addEventListener("message", function (evt) {
+            if (evt.data.type === "MEIOSIS_RENDER_MODEL") {
+                sendValues_1(evt.data.sendValuesBack);
+                params.streams[0](evt.data.model);
+            }
+            else if (evt.data.type === "MEIOSIS_TRACER_INIT") {
+                devtoolInitialized_1 = true;
+                bufferedValues_1.forEach(function (values) { return window.postMessage({ type: "MEIOSIS_VALUES", values: values }, "*"); });
+            }
+        });
+        lastStream.map(function () {
+            var change = changes_1();
+            var update = change !== lastChange_1;
+            lastChange_1 = change;
+            var values = params.streams.map(function (stream) {
+                return ({ value: copy_1(stream()) });
+            });
+            if (sendValues_1()) {
+                if (devtoolInitialized_1) {
+                    window.postMessage({ type: "MEIOSIS_VALUES", values: values, update: update }, "*");
+                }
+                else {
+                    bufferedValues_1.push(values);
+                }
+            }
+        });
+    }
+}
+exports.trace = trace;
+;
 
 
 /***/ }),
