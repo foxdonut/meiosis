16f7422a3477e992a2a2154f807ede03b549f097
foxdonut <fdaoud@fastmail.com>
2018-05-25 15:43:03 -0400

    Working on Mithril Router

--- wiki/code/04-Routing/C-Mithril-Router/app.js
+++ wiki/code/04-Routing/C-Mithril-Router/app.js
@@ -1,24 +1,22 @@
-/* global m, O */
+/* global m, O, pathToRegexp */
 
 const mlink = { oncreate: m.route.link, onupdate: m.route.link };
 
-// eslint-disable-next-line no-unused-vars
-const createHome = update => {
-  const page = {
-    id: "Home",
-    tab: "Home",
-    label: "Home"
-  };
-  const route = "/";
-
-  return {
-    href: () => route,
-    navigateTo: _params => update({ page }),
-    page,
-    route,
-    view: _vnode => m("div", "Home Page")
-  };
+const addHref = pageObject => {
+  pageObject.href = pathToRegexp.compile(pageObject.route);
+  return pageObject;
 };
+
+const HomePage = addHref({
+  tab: "Home",
+  label: "Home",
+  route: "/"
+});
+
+// eslint-disable-next-line no-unused-vars
+const createHome = update => O({
+  view: _vnode => m("div", "Home Page")
+}, HomePage);
 
 const coffees = [
   { id: "c1", description: "Coffee 1" },
@@ -29,76 +27,58 @@ const loadCoffees = () => new Promise(resolve =>
   setTimeout(() => resolve(coffees), 1)
 );
 
-// eslint-disable-next-line no-unused-vars
-const createCoffee = update => {
-  const page = {
-    id: "Coffee",
-    tab: "Coffee",
-    label: "Coffee"
-  };
-  const route = "/coffee";
+const CoffeePage = addHref({
+  tab: "Coffee",
+  label: "Coffee",
+  route: "/coffee/:id"
+});
 
-  return {
-    href: () => route,
-    navigateTo: _params => loadCoffees().then(coffees => update({ page, coffees })),
-    page,
-    route,
-    view: vnode => {
-      const model = vnode.attrs.model;
-      return (
-        m("div",
-          m("p", "Coffee Page"),
-          model.coffees.map(
-            coffee =>
-              m("span", { key: coffee.id },
-                m("a", { href: "#/coffee/" + coffee.id }, coffee.id),
-                " ",
-                model.coffee
-              )
-          )
+const coffeeView = model =>
+  m("div",
+    m("p", "Coffee Page"),
+    model.coffees.map(
+      coffee =>
+        m("span", { key: coffee.id },
+          m("a", O({ href: CoffeePage.href({ id: coffee.id }) }, mlink), coffee.id)
         )
-      );
-    }
-  };
-};
+    ),
+    model.coffee
+  );
 
-/*
-const Beer = {
-  page: {
-    id: "Beer",
-    tab: "Beer"
-  },
-  view: _vnode => m("div", "Beer Page")
-};
-*/
+// eslint-disable-next-line no-unused-vars
+const createCoffee = update => O({
+  navigateTo: params => loadCoffees().then(
+    coffees => update({ coffees, coffee: "Coffee " + params.id })
+  ),
+  view: vnode => coffeeView(vnode.attrs.model)
+}, CoffeePage);
 
-const navigateTo = component => {
-  const result = component.navigateTo();
-  if (result && typeof result.then === "function") {
-    result.then(() => m.redraw());
-  }
+const CoffeesPage = addHref({
+  tab: "Coffee",
+  label: "Coffee",
+  route: "/coffee"
+});
+
+// eslint-disable-next-line no-unused-vars
+const createCoffees = update => {
+  return O({
+    navigateTo: _params => loadCoffees().then(coffees => update({ coffees, coffee: O })),
+    view: vnode => coffeeView(vnode.attrs.model)
+  }, CoffeesPage);
 };
 
 // eslint-disable-next-line no-unused-vars
-const createLayout = components => ({
+const Layout = {
   view: vnode => {
-    const model = vnode.attrs.model;
-    const currentTab = model.page.tab;
-    const isActive = tab => tab === currentTab ? "active" : "";
+    const isActive = tab => tab === vnode.attrs.currentTab ? "active" : "";
 
     return (
       m("div",
         m("nav.navbar.navbar-default",
           m("ul.nav.navbar-nav",
-            components.map(component =>
-              m("li", { class: isActive(component.page.tab) },
-                m("a", O({ href: component.href() }, mlink), component.page.label)
-              )
-            ),
-            components.map(component =>
-              m("li.btn",
-                m("button.btn.btn-default",
-                  { onclick: _evt => navigateTo(component) }, component.page.label)
+            [HomePage, CoffeesPage].map(Component =>
+              m("li", { class: isActive(Component.tab) },
+                m("a", O({ href: Component.href() }, mlink), Component.label)
               )
             )
           )
@@ -107,35 +87,4 @@ const createLayout = components => ({
       )
     );
   }
-});
-
-/*
-const createBeerDetails = _update => ({
-  view: model => m("p", "Details of beer ", model.params.id)
-});
-
-const createBeer = (update, navigation) => {
-  const actions = {
-    beerDetails: id => _evt => navigation.navigateToBeerDetails({ id }),
-  };
-
-  return {
-    view: model =>
-      m("div",
-        m("p", "Beer Page"),
-        m("ul",
-          model.beerList.map(beer =>
-            m("li", { key: beer.id },
-              m("a", { href: "#/beer/" + beer.id }, beer.title),
-              " ",
-              m("button.btn.btn-default.btn-xs",
-                { onclick: actions.beerDetails(beer.id) },
-                beer.title
-              )
-            )
-          )
-        )
-      )
-  };
 };
-*/
--- wiki/code/04-Routing/C-Mithril-Router/index.js
+++ wiki/code/04-Routing/C-Mithril-Router/index.js
@@ -1,71 +1,21 @@
-/* global m, O, createLayout, createHome, createCoffee, meiosis, meiosisTracer */
+/* global m, O, Layout, createHome, createCoffees, createCoffee */
 
 const update = m.stream();
 const models = m.stream.scan(O, {}, update);
 
-const componentCreators = [ createHome, createCoffee ];
-const components = componentCreators.map(create => create(update));
-const defaultComponent = components[0];
-const Layout = createLayout(components);
+const render = Component => () =>
+  m(Layout, { currentTab: Component.tab }, m(Component, { model: models() }));
 
-/*
-const wrap = Component => ({
-  //onmatch: (args, _requestedPath) => Component.navigateTo(args),
-  onmatch: (args, _requestedPath) => {
-    return Component.navigateTo(args);
-  },
-  render: () => {
-    const model = models();
-    return m(Layout, { model }, m(Component, { model }));
-  }
+const createRouteResolver = Component => ({
+  onmatch: (args, _requestedPath) => Component.navigateTo && Component.navigateTo(args),
+  render: render(Component)
 });
-*/
 
-const componentMap = components.reduce((result, component) => {
-  result[component.page.id] = component;
-  return result;
-}, {});
+const routes = [ createHome, createCoffees, createCoffee ]
+  .map(create => create(update))
+  .reduce((result, Component) => {
+    result[Component.route] = createRouteResolver(Component);
+    return result;
+  }, {});
 
-const render = {
-  render: () => {
-    const model = models();
-    const Component = componentMap[(model.page.id || defaultComponent.page.id)] || defaultComponent;
-    return m(Layout, { model }, m(Component, { model }));
-  }
-};
-
-const wrap = Component => O({
-  //onmatch: (args, _requestedPath) => Component.navigateTo(args),
-  onmatch: (args, _requestedPath) => {
-    return Component.navigateTo(args);
-  },
-}, render);
-
-const routes = components.reduce((result, Component) => {
-  result[Component.route] = wrap(Component);
-  return result;
-}, {});
-
-const routeMap = components.reduce((result, Component) => {
-  result[Component.page.id] = Component.href;
-  return result;
-}, {});
-
-const routeSync = model => {
-  if (Object.keys(model).length > 0) {
-    const path = (model.page && (routeMap[model.page.id] || defaultComponent.href) || defaultComponent.href)();
-    if (document.location.hash.substring(2) !== path) {
-      //m.route.set(path);
-      window.history.pushState({}, "", "#!" + path);
-    }
-  }
-};
-models.map(routeSync);
-
-const element = document.getElementById("app");
-m.route(element, "/", routes);
-
-// Only for using Meiosis Tracer in development.
-meiosis.trace({ update, dataStreams: [ models ], toUpdate: state => state });
-meiosisTracer({ selector: "#tracer" });
-models.map(m.redraw);
+m.route(document.getElementById("app"), "/", routes);
