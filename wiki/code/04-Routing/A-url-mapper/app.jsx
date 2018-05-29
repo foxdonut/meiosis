8ed6f53971a82aff9c32d75d6afbeac8cc57b223
Graham Mendick <graham.mendick@gmail.com>
2018-05-29 11:35:26 +0100

    Built link to coffee details

--- wiki/code/04-Routing/A-url-mapper/app.jsx
+++ wiki/code/04-Routing/A-url-mapper/app.jsx
@@ -35,7 +35,7 @@ const createCoffee = (update, stateNavigator) => ({
     <div>
       <p>Coffee Page</p>
       {model.coffees.map(coffee => <span key={coffee.id}>
-        <a href={"#/coffee/" + coffee.id}>{coffee.id}</a>
+        <a href={`#${stateNavigator.getNavigationLink('coffee', { id: coffee.id })}`}>{coffee.id}</a>
         {" "}
       </span>)}
       {model.coffee}
