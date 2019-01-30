55dd4a42d793a49e1a0218a90cf3f2d06c16c0db
foxdonut <fdaoud@fastmail.com>
2019-01-30 07:21:33 -0500

    cleanup

--- docs/code/04-Routing/A-Navigation-Without-Routes/dev.js
+++
@@ -1,9 +0,0 @@
-/* global element, models, meiosisTracer */
-
-// For development only, this code sets up the Meiosis Tracer.
-const tracerElement = document.createElement("div");
-tracerElement.id = "tracer";
-tracerElement.style = "position: absolute; top: 0; right: 0";
-element.parentNode.insertBefore(tracerElement, element.nextSibling);
-
-meiosisTracer({ selector: "#tracer", streams: [ models ] });
