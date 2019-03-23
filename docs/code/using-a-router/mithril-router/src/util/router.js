aefdb4a9a8d1417d8a8021279f425d0a1abdb95a
foxdonut <fdaoud@fastmail.com>
2019-03-23 07:06:00 -0400

    working on routing

--- docs/code/using-a-router/mithril-router/src/util/router.js
+++ docs/code/using-a-router/mithril-router/src/util/router.js
@@ -1,5 +1,5 @@
 import m from "mithril";
-//import pathToRegexp from "path-to-regexp";
+import pathToRegexp from "path-to-regexp";
 
 import { Route } from "routing-common/src/root";
 
@@ -21,40 +21,44 @@ const routeMap = [
   { path: "/coffee", route: Route.Coffee, children: beverageRoutes },
   { path: "/beer", route: Route.Beer, children: beverageRoutes }
 ];
-
-const beverageRoutes = {
-  Route.Beverages,
-  Route.Beverage: { path: "/:id", children: {
-    Route.Brewer: { path: "/brewer" }
-  } }
-}
-
-const routeMap = {
-  Route.Home: { path: "/" },
-  Route.Login: { path: "/login" },
-  Route.Settings: { path: "/settings" },
-  Route.Tea: { path: "/tea", children: {
-    Route.TeaDetails: { path: "/:id" }
-  } },
-  Route.Coffee: { path: "/coffee", children: beverageRoutes },
-  Route.Beer: { path: "/beer", children: beverageRoutes }
-}
 */
 
+const beverageRoutes = {
+  Beverages: { path: "" },
+  Beverage: { path: "/:id", children: {
+    Brewer: { path: "/brewer" }
+  } }
+};
+
 const routeMap = {
-  "/": () => Route.Home(),
-  "/login": () => Route.Login(),
-  "/settings": () => Route.Settings(),
-  "/tea": () => Route.Tea(),
-  "/tea/:id": ({ id }) => Route.Tea({ child: Route.TeaDetails({ id }) }),
-  "/coffee": () => Route.Coffee({ child: Route.Beverages() }),
-  "/coffee/:id": ({ id }) => Route.Coffee({ child: Route.Beverage({ id }) }),
-  "/coffee/:id/brewer":
-    ({ id }) => Route.Coffee({ child: Route.Beverage({ id, child: Route.Brewer() }) }),
-  "/beer": () => Route.Beer({ child: Route.Beverages() }),
-  "/beer/:id": ({ id }) => Route.Beer({ child: Route.Beverage({ id }) }),
-  "/beer/:id/brewer":
-    ({ id }) => Route.Beer({ child: Route.Beverage({ id, child: Route.Brewer() }) })
+  Home: { path: "/" },
+  Login: { path: "/login" },
+  Settings: { path: "/settings" },
+  Tea: { path: "/tea", children: {
+    TeaDetails: { path: "/:id" }
+  } },
+  Coffee: { path: "/coffee", children: beverageRoutes },
+  Beer: { path: "/beer", children: beverageRoutes }
+};
+
+const createPathMap = routeMap => Object.entries(routeMap)
+  .reduce((result, [id, config]) => {
+    result[id] = {
+      toPath: pathToRegexp.compile(config.path),
+      children: createPathMap(config.children)
+    };
+    return result;
+  }, {});
+
+const pathMap = createPathMap(routeMap);
+
+const convertToPath = (route, localPathMap = pathMap) => {
+  const localPathDef = localPathMap[route.case];
+  let path = localPathDef.toPath(route.value);
+  if (route.child) {
+    path += convertToPath(route.child, localPathDef.children);
+  }
+  return path;
 };
 
 /*
@@ -70,21 +74,13 @@ Route.Beer({
 
 */
 
-/*
-const toPathMap = Object.entries(routeMap).reduce((result, [id, path]) => {
-  result[id] = pathToRegexp.compile(path);
-  return result;
-}, {});
-
 const getPath = () => document.location.hash;
 const setPath = path => window.history.pushState({}, "", path);
-*/
 
 // converts { case, value } to path
-// export const toPath = route => "#!" + toPathMap[route.case](route.value);
+export const toPath = route => "#!" + convertToPath(route);
 
 // Keeps the location bar in sync
-/*
 export const LocationBarSync = ({ state }) => {
   if (state.route.case) {
     const path = toPath(state.route);
@@ -94,7 +90,18 @@ export const LocationBarSync = ({ state }) => {
   }
   return null;
 };
-*/
+
+const createRoute = (id, config, path = "", result = {}) => {
+  const localPath = path + config.path;
+  result[localPath] = value => Route[id]({ value });
+
+  Object.entries(config.children || {}).forEach(([childId, childConfig]) => {
+    const childPath = localPath + childConfig.path;
+    result[childPath] = value => Route[id]({ value, child: Route[childId]({ value }) });
+  });
+
+  return result;
+};
 
 export const createRoutes = ({ states, actions, App }) =>
   Object.entries(routeMap).reduce((result, [path, fn]) => {
