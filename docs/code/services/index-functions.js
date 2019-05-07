c0b53df21297f4611661fcba83ee85737ad01e50
foxdonut <fdaoud@fastmail.com>
2019-05-06 23:17:50 -0400

    Update services

--- docs/code/services/index-functions.js
+++ docs/code/services/index-functions.js
@@ -1,8 +1,8 @@
 /* global b, m, R */
 
 const I = x => x;
-const o = (f, g) => (...args) => f(g(...args));
-const K = x => () => x;
+const T = (x, f) => f(x);
+const compose = (f, g) => (...args) => f(g(...args));
 
 const humanList = s => xs =>
   xs.length > 1
@@ -13,94 +13,7 @@ const humanList = s => xs =>
       xs.slice(-1)
     : xs.join("");
 
-const pipe = xs => xs.reduceRight(o, I);
-const $ = {
-  prop: k => f => o => Object.assign(o, { [k]: f(o[k]) }),
-  get: lens => o => {
-    var y;
-    lens(x => (y = x))(o);
-    return y;
-  }
-};
-
-const $boxes = $.prop("boxes");
-const $description = $.prop("description");
-const $colors = $.prop("colors");
-
-const Action = {
-  addBox: x =>
-    pipe([K(x), x => xs => xs.concat(x), $boxes]),
-  removeBox: i => $boxes(xs => xs.filter((x, j) => i != j))
-};
-
-const view = update => state =>
-  m(
-    ".app" + b.d("grid").ff("Helvetica"),
-    m(
-      "nav.header" +
-        b
-          .d("flex")
-          .jc("space-between")
-          .ai("center")
-          .bc("steelblue")
-          .c("white")
-          .p("1em"),
-
-      m("h1" + b.m(0), "Boxes"),
-      $.get($colors)(state).map(x =>
-        m(
-          "button" +
-            b
-              .bc(x)
-              .c("white")
-              .w("2em")
-              .h("2em")
-              .fs("2em")
-              .m(0)
-              .border("none"),
-          { onclick: pipe([Action.addBox(x), update]) },
-          "+"
-        )
-      )
-    ),
-    m("p", $.get($description)(state)),
-    m(
-      "" +
-        b
-          .d("grid")
-          .gridTemplateColumns("repeat(3, 1fr)")
-          .alignItems("center")
-          .justifyItems("center")
-          .padding("1em")
-          .gridRowGap("1em")
-          .maxHeight("14em")
-          .overflowY("auto"),
-
-      $.get($boxes)(state).map((x, i) =>
-        m(
-          "" +
-            b
-              .bc(x)
-              .c("white")
-              .w("4em")
-              .h("4em"),
-          {
-            onclick: pipe([K(Action.removeBox(i)), update])
-          }
-        )
-      )
-    )
-  );
-
-const T = (x, f) => f(x);
-
-const StatsService = {
-  initial(state) {
-    return state.colors
-      .map(R.objOf)
-      .map(K(0))
-      .reduce(R.merge, {});
-  },
+const stats = {
   accept: R.pipe(
     x => x.boxes,
     R.countBy(I),
@@ -108,31 +21,21 @@ const StatsService = {
   )
 };
 
-const LocalStorageService = {
-  initial() {
-    return [localStorage.getItem("v1")]
-      .filter(Boolean)
-      .map(JSON.parse)
-      .concat({ boxes: [] })
-      .shift();
+const storage = {
+  Initial: () => {
+    const stored = localStorage.getItem("v1");
+    return stored ? JSON.parse(stored) : {};
   },
+
   service: ({ state }) => {
-    T(
-      state,
-      R.pipe(
-        R.pick(["boxes"]),
-        x => localStorage.setItem("v1", JSON.stringify(x))
-      )
+    localStorage.setItem(
+      "v1",
+      JSON.stringify({ boxes: state.boxes })
     );
   }
 };
 
-const DescriptionService = {
-  initial() {
-    return {
-      description: ""
-    };
-  },
+const description = {
   accept: R.pipe(
     x => x.stats,
     R.toPairs,
@@ -147,46 +50,113 @@ const DescriptionService = {
   )
 };
 
-const initial = [
-  LocalStorageService.initial,
-  StatsService.initial,
-  DescriptionService.initial
-];
+const app = {
+  Initial: () =>
+    Object.assign(
+      {
+        boxes: [],
+        colors: ["red", "purple", "blue"]
+      },
+      storage.Initial()
+    ),
 
-const acceptors = [
-  StatsService.accept,
-  DescriptionService.accept
-];
-
-const services = [LocalStorageService.service];
-
-const initialState = () => {
-  const state = {
-    boxes: [],
-    colors: ["red", "purple", "blue"]
-  };
-  return Object.assign(
-    {},
-    state,
-    initial.map(fn => fn(state)).reduce(R.merge, {})
-  );
+  Actions: update => ({
+    addBox: x =>
+      update(
+        R.over(R.lensProp("boxes"), xs => xs.concat(x))
+      ),
+    removeBox: i =>
+      update(
+        R.over(R.lensProp("boxes"), xs =>
+          xs.filter((x, j) => i != j)
+        )
+      )
+  })
 };
+
+const App = {
+  view: ({ attrs: { state, actions } }) =>
+    m(
+      ".app" + b.d("grid").ff("Helvetica"),
+      m(
+        "nav.header" +
+          b
+            .d("flex")
+            .jc("space-between")
+            .ai("center")
+            .bc("steelblue")
+            .c("white")
+            .p("1em"),
+
+        m("h1" + b.m(0), "Boxes"),
+        state.colors.map(x =>
+          m(
+            "button" +
+              b
+                .bc(x)
+                .c("white")
+                .w("2em")
+                .h("2em")
+                .fs("2em")
+                .m(0)
+                .border("none"),
+            { onclick: () => actions.addBox(x) },
+            "+"
+          )
+        )
+      ),
+      m("p", state.description),
+      m(
+        "" +
+          b
+            .d("grid")
+            .gridTemplateColumns("repeat(3, 1fr)")
+            .alignItems("center")
+            .justifyItems("center")
+            .padding("1em")
+            .gridRowGap("1em")
+            .maxHeight("14em")
+            .overflowY("auto"),
+
+        state.boxes.map((x, i) =>
+          m(
+            "" +
+              b
+                .bc(x)
+                .c("white")
+                .w("4em")
+                .h("4em"),
+            { onclick: () => actions.removeBox(i) }
+          )
+        )
+      )
+    )
+};
+
+const acceptors = [stats.accept, description.accept];
+const services = [storage.service];
 
 const accept = state =>
   acceptors.reduce(
     (updatedState, acceptor) =>
-      T(updatedState, acceptor(updatedState)),
+      acceptor(updatedState)(updatedState),
     state
   );
 
 const update = m.stream();
+const actions = app.Actions(update);
 const states = m.stream.scan(
-  o(accept, T),
-  accept(initialState()),
+  compose(
+    accept,
+    T
+  ),
+  accept(app.Initial()),
   update
 );
-const element = document.getElementById("app");
-states.map(view(update)).map(v => m.render(element, v));
+
+m.mount(document.getElementById("app"), {
+  view: () => m(App, { state: states(), actions })
+});
 
 states.map(state =>
   services.forEach(service => service({ state, update }))
