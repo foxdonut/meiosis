4e7abfadf0a07be2d0db1b5df51d69fdb0167dd1
foxdonut <fdaoud@fastmail.com>
2019-06-19 08:08:21 -0400

    Using TypeScript for meiosis-routing

--- helpers/routing/lib/router-helper/index.d.ts
+++ helpers/routing/lib/router-helper/index.d.ts
@@ -1,16 +1,23 @@
 /**
  * `meiosis-routing/router-helper`
- * @module router-helper
+ *
+ * The `router-helper` module contains functions for creating a router by plugging in a router
+ * library.
+ *
+ * @module routerHelper
  */
-import { Route } from "../state";
+/**
+ * Nested route configuration.
+ */
 export declare type NestedRouteConfig = [string, RouteConfig] | [string, string[], RouteConfig];
+import { Route } from "../state";
 /**
  * Route configuration. This is an Object for which the properties are the ids of the route
  * segments, and the values are either:
  *
  * - a string: the route path. May contain `:` for path parameters. May also contain `?` and/or `&`
  *   for query string parameters.
- * - an array: `[ path, (optional) array of parameters from the parent, nested route config ]`
+ * - an array: [[NestedRouteConfig]]
  *
  * @example
  *
@@ -29,63 +36,67 @@ export interface RouteConfig {
     [id: string]: string | NestedRouteConfig;
 }
 /**
- * `function parsePath(path, queryParams): route`
- *
  * Function that parses a path and returns a route.
  *
- * @typedef {function} parsePath
- *
- * @param {string} path - the path to parse.
- * @param {Object} queryParams - an object with the query string parameters, if any are present.
- * @returns {route} the route obtained from the path and parameters.
+ * @param path the path to parse.
+ * @param queryParams an object with the query string parameters, if any are present.
+ * @returns the route obtained from the path and parameters.
  */
 export declare type parsePath = (path: string, queryParams: Record<string, any>) => Route;
 /**
- * `function createParsePath(routeMap, defaultRoute): parsePath`
- *
  * Function that creates a function to parse a path.
  *
- * @typedef {function} createParsePath
- *
- * @param {Object} routeMap - an object with key-value pairs.
- * @param {Route} defaultRoute - the default route.
- * @returns {parsePath} the function that parses a path.
+ * @param routeMap an object with key-value pairs.
+ * @param defaultRoute the default route.
+ * @returns the function that parses a path.
  */
 export declare type createParsePath = (routeMap: RouteMap, defaultRoute?: Route) => parsePath;
 /**
  * Router configuration.
- *
- * @property {RouteConfig} routeConfig - the route config
- * @property {string} [prefix="#"] - the URL path prefix. Defaults to `"#"`.
- * @property {Route} [defaultRoute] - the default route
- * @property {createParsePath} createParsePath - function that parses a path using a router library.
- * @property {function} [getPath] - the function to get the path from the browser's location bar.
- * Defaults to `(() => document.location.hash || prefix + "/")`.
- * @property {function} [setPath] - the function to set the path on the browser's location bar.
- * Defaults to `(path => window.history.pushState({}, "", path))`.
- * @property {function} [addLocationChangeListener] - the function to add the location change
- * listener. Defaults to `window.onpopstate = listener`.
- * @property {function} createRouteMatcher - the Feather Route Matcher function.
- * @property {Function} Mapper - the URL Mapper function.
- * @property {Mithril} m - the Mithril instance.
  */
 export interface RouterConfig {
+    /** The route config. */
     routeConfig: RouteConfig;
+    /** The URL path prefix. Defaults to `"#"`. */
     prefix?: string;
+    /** The default route. */
     defaultRoute?: Route;
+    /** Function that parses a path using a router library. */
     createParsePath?: createParsePath;
     queryString?: any;
+    /**
+     * The function to get the path from the browser's location bar.
+     * Defaults to `(() => document.location.hash || prefix + "/")`.
+     */
     getPath: () => string;
+    /**
+     * The function to set the path on the browser's location bar.
+     * Defaults to `(path => window.history.pushState({}, "", path))`.
+     */
     setPath: (path: string) => void;
+    /**
+     * The function to add the location change listener. Defaults to `window.onpopstate = listener`.
+     */
     addLocationChangeListener?: any;
+    /**
+     * The [Feather Route Matcher](https://github.com/henrikjoreteg/feather-route-matcher)
+     * function.
+     */
     createRouteMatcher?: any;
+    /** The [URL Mapper](https://github.com/cerebral/url-mapper) function. */
     Mapper?: any;
+    /** The [Mithril](https://mithril.js.org) instance. */
     m?: any;
 }
+/** Represents a function that takes params and produces a [[Route]]. */
 export declare type RouteFn = (params: Record<string, any>) => Route;
+/** Object that maps paths to route functions. */
 export interface RouteMap {
     [path: string]: RouteFn;
 }
+/**
+ * Represents a router, which provides properties and functions to manage route paths.
+ */
 export interface Router {
     initialRoute?: Route;
     locationBarSync: (route: Route) => void;
@@ -114,8 +125,8 @@ export declare function createRouteMap(routeConfig?: {}, path?: string, fn?: (pa
  * `function createParsePath(routeMap, defaultRoute)` receives a `routeMap` which is an object
  * with
  *
- * @param {RouterConfig} config
- * @returns {Object} router
+ * @param config
+ * @returns the created router.
  *
  * @example
  *
@@ -138,12 +149,12 @@ export declare function createRouteMap(routeConfig?: {}, path?: string, fn?: (pa
  * };
  * ```
  */
-export declare function createRouter({ createParsePath, routeConfig, defaultRoute, prefix, queryString, getPath, setPath, addLocationChangeListener }: RouterConfig): Router;
+export declare function createRouter(config: RouterConfig): Router;
 /**
  * Creates a router using
  * [feather-route-matcher](https://github.com/HenrikJoreteg/feather-route-matcher).
  *
- * @param {FeatherConfig} config
+ * @param config
  *
  * @example
  *
@@ -164,21 +175,12 @@ export declare function createRouter({ createParsePath, routeConfig, defaultRout
  * });
  * ```
  */
-export declare function createFeatherRouter({ createRouteMatcher, routeConfig, defaultRoute, prefix, queryString, getPath, setPath, addLocationChangeListener }: {
-    createRouteMatcher: any;
-    routeConfig: any;
-    defaultRoute: any;
-    prefix?: string | undefined;
-    queryString: any;
-    getPath: any;
-    setPath: any;
-    addLocationChangeListener: any;
-}): Router;
+export declare function createFeatherRouter(config: RouterConfig): Router;
 /**
  * Creates a router using
  * [url-mapper](https://github.com/cerebral/url-mapper).
  *
- * @param {UrlMapperConfig} config
+ * @param config
  *
  * @example
  *
@@ -199,20 +201,11 @@ export declare function createFeatherRouter({ createRouteMatcher, routeConfig, d
  * });
  * ```
  */
-export declare function createUrlMapperRouter({ Mapper, routeConfig, defaultRoute, prefix, queryString, getPath, setPath, addLocationChangeListener }: {
-    Mapper: any;
-    routeConfig: any;
-    defaultRoute: any;
-    prefix?: string | undefined;
-    queryString: any;
-    getPath: any;
-    setPath: any;
-    addLocationChangeListener: any;
-}): Router;
+export declare function createUrlMapperRouter(config: RouterConfig): Router;
 /**
  * Creates a router using [Mithril Router](https://mithril.js.org/route.html).
  *
- * @param {MithrilConfig} config
+ * @param config
  *
  * @example
  *
@@ -231,10 +224,4 @@ export declare function createUrlMapperRouter({ Mapper, routeConfig, defaultRout
  * });
  * ```
  */
-export declare function createMithrilRouter({ m, routeConfig, prefix, getPath, setPath }: {
-    m: any;
-    routeConfig: any;
-    prefix?: string | undefined;
-    getPath: any;
-    setPath: any;
-}): Router;
+export declare function createMithrilRouter(config: RouterConfig): Router;
--- helpers/routing/lib/state/index.d.ts
+++ helpers/routing/lib/state/index.d.ts
@@ -1,5 +1,8 @@
 /**
  * `meiosis-routing/state`
+ *
+ * The `state` module contains functions for managing routes in the application state.
+ *
  * @module state
  */
 /**
@@ -9,35 +12,59 @@ export interface RouteSegment {
     id: string;
     params: Record<string, any>;
 }
+/**
+ * A function that creates a [[RouteSegment]] with optional params.
+ */
 export declare type RouteParamFn = (params: Record<string, object> | null) => RouteSegment;
 /**
  * A Route is an array of route segments.
  */
 export declare type Route = RouteSegment[];
+/**
+ * The route state.
+ */
 export interface RouteState {
     previous: Route;
     current: Route;
 }
+/**
+ * The route transition indicates the [[Route]] that the user is leaving and to which they are
+ * arriving.
+ */
 export interface RouteTransition extends RouteState {
     leave: Route;
     arrive: Route;
 }
+/**
+ * Routing instance with navigation objects and functions.
+ */
 export interface RoutingObject {
+    /** the current [[Route]]. */
     route: Route;
+    /** the route index, used internally. */
     index: number;
+    /** the [[RouteSegment]] for the local route. */
     localSegment: RouteSegment;
+    /** the [[RouteSegment]] for the child route. */
     childSegment: RouteSegment;
+    /** returns the [[RoutingObject]] for the next child. */
     next: () => RoutingObject;
+    /** returns the parent [[Route]]. */
     parentRoute: () => Route;
+    /** returns the [[Route]] for the current route plus the given child route. */
     childRoute: (child: Route) => Route;
+    /** returns the [[Route]] for the current route plus the given sibling route. */
     siblingRoute: (sibling: Route) => Route;
 }
 /**
  * Creates a `Route` helper with functions to create route segments.
- * @param {Array<string>} routeNames - the list of route names.
- * @returns {Constructor<RouteSegment>} - the `Route` with constructor functions.
+ *
+ * @param routeNames the list of route names.
+ * @returns a `Route` object with constructor functions.
  *
  * @example
+ *
+ * ```
  *
  * const Route = createRouteSegments(["Home", "User"]);
  *
@@ -46,47 +73,92 @@ export interface RoutingObject {
  *
  * Route.User({ name: "duck" })
  * // { id: "User", params: { name: "duck" } }
+ * ```
  */
 export declare function createRouteSegments(routeNames: string[]): Record<string, RouteParamFn>;
 /**
  * Looks for a route segment, with matching params, in a route.
- * @param {Route} route
- * @param {RouteSegment} routeSegmentWithParams
- * @returns {RouteSegment} - the matching Route segment, or `undefined` if `route` is empty or the
- * Route segment was not found.
+ *
+ * @param route the route to search.
+ * @param routeSegmentWithParams the route segment to search for in the route.
+ * @returns the matching Route segment, or `undefined` if `route` is empty or the route segment
+ * was not found.
  */
 export declare function findRouteSegmentWithParams(route: Route, routeSegmentWithParams: RouteSegment): RouteSegment | undefined;
 /**
  * Looks for a Route segment, regardless of the params, in a route.
- * @param {Route} route
- * @param {string} id
- * @returns {RouteSegment} - the matching Route segment, or `undefined` if `route` is empty or a
- * route segment with the given id was not found.
+ *
+ * @param route the route to search.
+ * @param id the route segment, or just the id of the route segment, to search for in the route.
+ * @returns the matching Route segment, or `undefined` if `route` is empty or a route segment with
+ * the given id was not found.
  */
 export declare function findRouteSegment(route: Route, id: RouteSegment): RouteSegment | undefined;
+/**
+ * Calculates the difference between two routes.
+ *
+ * @param from
+ * @param to
+ * @returns the route representing the segments that are in the `from` route but not in the `to`
+ * route.
+ */
 export declare function diffRoute(from: Route, to: Route): Route;
 /**
  * Calculates route transitions, providing `leave` and `arrive` to indicate the route segments for
  * the route that we are leaving, and the route to which we are arriving, respectively.
- * @param {Object} state the route state
- * @returns {Object} an object with `previous`, `current`, `leave`, and `arrive` properties.
- */
-export declare function routeTransition({ previous, current }: RouteState): RouteTransition;
-/**
- * `function whenPresent(value, fn): any`
  *
+ * @param state the route state.
+ * @returns an object with `previous`, `current`, `leave`, and `arrive` properties.
+ */
+export declare function routeTransition(routeState: RouteState): RouteTransition;
+/**
  * Calls a function with a value only if the value is not `null` or `undefined`.
- * @param {*} value the value to check
- * @param {function(value)} fn the function to call if `value` is present
- * @returns {*} - the result of calling `fn(value)`, or `null` if `value` is absent.
+ *
+ * @param value the value to check.
+ * @param fn the function to call if `value` is present.
+ * @returns the result of calling `fn(value)`, or `null` if `value` is absent.
  */
 export declare function whenPresent(value: any, fn: (x: any) => any): any;
 /**
- * @constructor Routing
+ * @constructor [[RoutingObject]]
  *
- * @param {Route} route
- * @param {number} index
+ * @param route the current route, for example `state.route.current`.
+ * @param index the route segment index. This is used internally and you should not specify a value
+ * for this parameter.
  *
- * @returns {routing} - a routing object
+ * @example
+ *
+ * ```
+ *
+ * // in root component
+ * const Root = ({ state }) => {
+ *   const routing = Routing(state.route.current);
+ *   const Component = componentMap[routing.localSegment.id];
+ *
+ *   return (
+ *     <div>
+ *       <Component // other props... // routing={routing} />
+ *     </div>
+ *   );
+ * };
+ *
+ * // in child component
+ * const Child = ({ state, routing }) => {
+ *   const Component = componentMap[routing.childSegment.id];
+ *   const params = routing.localSegment.params;
+ *
+ *   return (
+ *     <div>
+ *       <a href={router.toPath(routing.parentRoute())}>...</a>
+ *       <a href={router.toPath(routing.childRoute(Route.Child()))}>...</a>
+ *       <a href={router.toPath(
+ *         routing.siblingRoute([Route.Sibling(), Route.Details()])
+ *       )}>...</a>
+ *
+ *       <Component // other props... // routing={routing.next()} />
+ *     </div>
+ *   );
+ * };
+ * ```
  */
 export declare function Routing(route?: Route, index?: number): RoutingObject;
