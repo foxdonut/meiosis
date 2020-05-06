19b0dd2db86638b1264f418f6969cb6ca4066888
foxdonut <fdaoud@fastmail.com>
2020-05-06 19:35:46 -0400

    update setup and routing examples

--- docs/code/routing-full/routing-common-core/src/settings/effect.js
+++
@@ -1,7 +0,0 @@
-import { navigateTo } from "../routes";
-
-export const effect = update => state => {
-  if (state.redirect) {
-    update([navigateTo(state.redirect), { redirect: undefined }]);
-  }
-};
--- docs/code/routing-full/routing-common-core/src/settings/service.js
+++ docs/code/routing-full/routing-common-core/src/settings/service.js
@@ -3,12 +3,14 @@ import { Route } from "../routes";
 export const service = state => {
   if (state.routeTransition.arrive.Settings && !state.user) {
     return {
-      route: [state.routeTransition.leave],
+      route: [Route.Login()],
       routeTransition: { arrive: () => ({}), leave: () => ({}) },
-      redirect: Route.Login({
-        message: "Please login.",
-        returnTo: Route.Settings()
-      })
+      redirect: [
+        Route.Login({
+          message: "Please login.",
+          returnTo: Route.Settings()
+        })
+      ]
     };
   }
 };
