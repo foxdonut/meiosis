6664d77e312b1cfc63bd265a9ff412dba303e5b8
foxdonut <fdaoud@fastmail.com>
2020-09-09 06:34:44 -0400

    working on router setup

--- docs/code/router-setup/history-mode/programmatic-url/src/router/index-with-lib.js
+++ docs/code/router-setup/history-mode/programmatic-url/src/router/index-with-lib.js
@@ -3,15 +3,16 @@ Instead of meiosis/router.js,
 you can also npm install meiosis-router-setup and use it as shown below:
 */
 
-import createRouteMatcher from "feather-route-matcher";
+import Mapper from "url-mapper";
 import { createRouter } from "meiosis-router-setup";
 import queryString from "query-string";
 import { routeConfig } from "router-setup-common/src/router";
 
-const routeMatcher = createRouteMatcher(routeConfig);
+const urlMapper = Mapper();
+const routeMatcher = path => urlMapper.map(path, routeConfig);
 const convertMatchToRoute = (match, queryParams) => ({
-  page: match.value,
-  params: match.params,
+  page: match.match,
+  params: match.values,
   queryParams
 });
 
