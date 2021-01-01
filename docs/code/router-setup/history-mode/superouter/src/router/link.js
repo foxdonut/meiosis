b35c0d843cd1c353af7dd08aadd6a16c4b0a4acb
foxdonut <fdaoud@fastmail.com>
2021-01-01 10:25:24 -0500

    working on router setup

--- docs/code/router-setup/history-mode/superouter/src/router/link.js
+++
@@ -1,5 +0,0 @@
-export const getLinkAttrs = (router, page, queryParams) => {
-  const url = router.toUrl(page, null, queryParams);
-
-  return { href: url, onclick: router.getLinkHandler(url) };
-};
