987a70099a59321a50df98231f2deb26ebba6226
foxdonut <fdaoud@fastmail.com>
2021-08-09 07:28:35 -0400

    working on meiosis-setup one

--- helpers/setup/source/src/util/index.d.ts
+++ helpers/setup/source/src/util/index.d.ts
@@ -1,3 +1,12 @@
+/**
+ * Safely gets a property path from an object. The path is an array. If any property along the path
+ * is `undefined`, the function returns `undefined`.
+ *
+ * @param {*} object the object on which to get the property.
+ * @param {Array<string>} path the property path.
+ *
+ * @returns {*} the property value, or `undefined` if any property along the path is `undefined`.
+ */
 export function get(object: any, path: Array<string>): any;
 
 export function setMutate(object: any, path: Array<string>, value: any): any;
--- helpers/setup/source/src/util/index.js
+++ helpers/setup/source/src/util/index.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 /** @type {import("./index").get} */
 export const get = (object, path) =>
   path.reduce((obj, key) => (obj == undefined ? undefined : obj[key]), object);
