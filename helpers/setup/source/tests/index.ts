b6335bd3d26761e781b14c9073812a30dd6d09c4
foxdonut <fdaoud@fastmail.com>
2021-12-04 13:57:00 -0500

    working on Meiosis Cell

--- helpers/setup/source/tests/index.ts
+++ helpers/setup/source/tests/index.ts
@@ -1,10 +1,34 @@
-// import meiosis from "../src/index";
+import simpleStream from "../src/simple-stream";
+import { meiosisCell, nest } from "../src/mergerino";
+import merge from "mergerino";
 
 describe("Meiosis with TypeScript", () => {
   describe("Meiosis Cell", () => {
     describe("Mergerino", () => {
-      test("basic with no actions", () => {
-        expect(2 + 2).toEqual(4);
+      test("basic with nesting and no actions", () => {
+        interface Duck {
+          color: string;
+        }
+
+        interface State {
+          duck: Duck;
+          sound: string;
+        }
+        const app = {};
+        const rootCell = meiosisCell<State, never>({ stream: simpleStream, merge, app });
+
+        expect(rootCell.actions).toBeUndefined();
+        expect(rootCell.getState()).toEqual({});
+
+        rootCell.update({ sound: "quack" });
+        expect(rootCell.getState()).toEqual({ sound: "quack" });
+
+        const duckCell = nest(rootCell, "duck");
+        expect(duckCell.getState()).toBeUndefined();
+
+        duckCell.update({ color: "yellow" });
+        expect(duckCell.getState()).toEqual({ color: "yellow" });
+        expect(rootCell.getState()).toEqual({ sound: "quack", duck: { color: "yellow" } });
       });
     });
   });
