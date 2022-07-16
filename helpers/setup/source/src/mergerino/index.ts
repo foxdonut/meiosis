863537cc278dc4566f7edeb8522418d2d23db604
foxdonut <fdaoud@fastmail.com>
2022-07-16 10:25:01 -0400

    update ts docs

--- helpers/setup/source/src/mergerino/index.ts
+++ helpers/setup/source/src/mergerino/index.ts
@@ -219,15 +219,75 @@ const intoPath = (path: string[], value: string | number): any => ({
   [path[0]]: path.length === 1 ? value : intoPath(path.slice(1), value)
 });
 
+/**
+ * Convenience function to update a form value. Pass the Meiosis cell and the state property (such
+ * as `'firstName'`) or path (such as `['person', 'firstName']`) into which to update the value.
+ * Returns a function that you can pass to a DOM handler, such as `oninput` (Mithril) or `onInput`
+ * (Preact, React). For example:
+ *
+ * ```js
+ * // Using Mithil
+ * m('input[type=text]', { oninput: updateFormValue(cell, 'firstName') })
+ *
+ * // Using Preact/React
+ * <input type="text" onInput={updateFormValue(cell, ['person', 'firstName'])}/>
+ * ```
+ *
+ * @param cell the Meiosis cell.
+ * @param path the property or path into which to update the value.
+ * @param fn (optional) a function to modify the value before updating it.
+ *
+ * @returns a function that accepts a DOM event and updates the value on the Meiosis state.
+ */
 export const updateFormValue = (
   cell: MeiosisCell<any>,
   path: string[] | string,
   fn: (value: string) => any = (value) => value
 ) => updateStringValueIntoPath(intoPath, cell, path, fn);
 
+/**
+ * Convenience function to update a form value with an Integer value. If the user input does not
+ * return a number with `parseInt`, no state change occurs. Pass the Meiosis cell and the state
+ * property (such as `'counter'`) or path (such as `['book', 'counter']`) into which to update the
+ * value. Returns a function that you can pass to a DOM handler, such as `oninput` (Mithril) or
+ * `onInput` (Preact, React). For example:
+ *
+ * ```js
+ * // Using Mithil
+ * m('input[type=text]', { oninput: updateFormIntValue(cell, 'counter') })
+ *
+ * // Using Preact/React
+ * <input type="text" onInput={updateFormIntValue(cell, ['book', 'counter'])}/>
+ * ```
+ *
+ * @param cell the Meiosis cell.
+ * @param path the property or path into which to update the value.
+ *
+ * @returns a function that accepts a DOM event and updates the value on the Meiosis state.
+ */
 export const updateFormIntValue = (cell: MeiosisCell<any>, path: string[] | string) =>
   updateIntValueIntoPath(intoPath, cell, path);
 
+/**
+ * Convenience function to update a form value with a Float value. If the user input does not return
+ * a number with `parseFloat`, no state change occurs. Pass the Meiosis cell and the state property
+ * (such as `'pH'`) or path (such as `['water', 'pH']`) into which to update the value. Returns a
+ * function that you can pass to a DOM handler, such as `oninput` (Mithril) or `onInput` (Preact,
+ * React). For example:
+ *
+ * ```js
+ * // Using Mithil
+ * m('input[type=text]', { oninput: updateFormFloatValue(cell, 'pH') })
+ *
+ * // Using Preact/React
+ * <input type="text" onInput={updateFormFloatValue(cell, ['water', 'pH'])}/>
+ * ```
+ *
+ * @param cell the Meiosis cell.
+ * @param path the property or path into which to update the value.
+ *
+ * @returns a function that accepts a DOM event and updates the value on the Meiosis state.
+ */
 export const updateFormFloatValue = (cell: MeiosisCell<any>, path: string[] | string) =>
   updateFloatValueIntoPath(intoPath, cell, path);
 
