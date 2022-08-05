df9ba6546d1da399d37b556df76449b282ba9531
foxdonut <fdaoud@fastmail.com>
2022-08-04 20:51:10 -0400

    working on docs

--- helpers/setup/examples/src/ts/support/nested-cells.ts
+++ helpers/setup/examples/src/ts/support/nested-cells.ts
@@ -13,8 +13,8 @@ const cells = meiosisSetup<State>();
 const cell = cells();
 cell;
 
-// Uncomment the next line and pass a string to nest().
-// Notice that auto-suggest indicates valid properties,
+// Uncomment the next line and pass an emptry string to nest().
+// Press Ctrl+Space and notice that auto-suggest indicates valid properties,
 // and that specifying an invalid property results in an error.
 // Specify 'phone' to create a nested cell.
 // const nestedCell = cell.nest();
@@ -23,6 +23,6 @@ cell;
 // indicates 'home' and 'work'.
 // nestedCell.state;
 
-// Enter an empty object {} within update(), notice that auto-suggest
-// indicates 'home' and 'work'.
+// Enter an empty object {} within update() and press Ctrl+Space.
+// Notice that auto-suggest nested properties of `phone`.
 // nestedCell.update();
