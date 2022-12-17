613c2bbedfdbf4b1482ac8633618d2a13ab29187
foxdonut <fdaoud@fastmail.com>
2022-12-17 13:31:40 -0500

    working on router

--- helpers/router/examples/hash-mode/src/login/service.js
+++ helpers/router/examples/hash-mode/src/login/service.js
@@ -3,17 +3,20 @@ import { Route, router } from '../router';
 export const service = {
   onchange: (state) => state.route.value,
   run: (cell) => {
-    if (
-      cell.state.route.value !== Route.Login &&
-      (cell.state.login.username || cell.state.login.password)
-    ) {
+    if (cell.state.route.value !== Route.Login) {
       if (
-        !cell.state.user &&
-        !confirm('You have unsaved data. Continue?')
-      ) {
+        (cell.state.login.username || cell.state.login.password) &&
+        (!cell.state.user && !confirm('You have unsaved data. Continue?'))) {
         cell.update({ route: () => router.toRoute(Route.Login) });
       } else {
-        cell.update({ login: { username: '', password: '' } });
+        cell.update({
+          login: {
+            username: '',
+            password: '',
+            message: '',
+            returnTo: undefined
+          }
+        });
       }
     }
   }
