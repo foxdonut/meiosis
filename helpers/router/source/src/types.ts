94fd6ebfc2c3805f23ccc6e90c70010d2b6f707f
foxdonut <fdaoud@fastmail.com>
2023-01-01 08:45:24 -0500

    working on router

--- helpers/router/source/src/types.ts
+++ helpers/router/source/src/types.ts
@@ -24,7 +24,7 @@
  * export const router = createRouter({ routeConfig });
  * ```
  *
- * This will provide type safety for route values.
+ * This will provide auto-suggest and type safety for route values.
  *
  * ```ts
  * router.toRoute('invalid') // error
@@ -40,23 +40,30 @@ export type Params = Record<string, any>;
 
 /**
  * A route in the application state.
+ *
+ * @template T See {@link RouteConfig} for details.
+ *
  */
 export type Route<T extends string = string> = {
-  /** The value corresponding to the route. */
+  /** The route value. */
   value: T;
 
-  /** An object with route and query string params. */
+  /** The path and query parameters. */
   params: Params;
 
-  /** Indicates whether to replace the entry in the browser's history. */
+  /** Indicates whether to replace the entry in the browser's history. See {@link ToRoute} for more
+   * details. */
   replace?: boolean;
 };
 
 /**
  * Function to generate a URL from a value and params.
  *
+ * @template T See {@link RouteConfig} for details.
+ *
  * @param value the route value.
- * @param params the path parameters.
+ * @param params the path parameters and, optionally query parameters. Any parameters not part of
+ * path parameters are automatically added as query parameters.
  *
  * @returns the URL.
  */
@@ -65,8 +72,13 @@ export type ToUrl<T extends string = string> = (value: T, params?: Params) => st
 /**
  * Function to generate a Route.
  *
+ * @template T See {@link RouteConfig} for details.
+ *
  * @param value the route value.
- * @param params the path parameters.
+ * @param params the route parameters.
+ * @param replace indicates whether to replace the entry in the browser's history instead of
+ * appending. This is useful, for example, when redirecting from a route that the user was not
+ * allowed to access.
  */
 export type ToRoute<T extends string = string> =
   (value: T, params?: Params, replace?: boolean) => Route<T>;
@@ -76,24 +88,26 @@ export type GetStatePath = (path: string) => string;
 export type SetHref = (href: string) => void;
 
 /**
- * Callback function for when the route changes. Typically, this function updates the application
- * state with the route, for example:
+ * Callback function for when the route changes. This function should be used to update the route in
+ * the application state. For example:
  *
  * ```js
- * router.start(route => update({ route: () => route }));
+ * router.start(route => cell.update({ route: () => route }));
  * ```
  *
- * @param route type current route.
+ * @param route the current route.
  */
 export type OnRouteChange<T extends string = string> = (route: Route<T>) => void;
 
 /**
  * Function to start the router.
  *
- * @param onRouteChange callback function for when the route changes.
+ * @param onRouteChange callback function for when the route changes. This should be used to update
+ * the route in the application state. See {@link OnRouteChange}.
  */
 export type Start<T extends string = string> = (onRouteChange: OnRouteChange<T>) => void;
 
+/** Do not use this, it is for internal testing purposes only. */
 export type WindowLike = {
   decodeURI: (uri: string) => string;
   history: {
@@ -124,6 +138,8 @@ export type SyncLocationBar<T extends string = string> = (route: Route<T>) => vo
 
 /**
  * Configuration to create a router.
+ *
+ * @template T See {@link RouteConfig} for details.
  */
 export type RouterConfig<T extends string = string> = {
   /** The route configuration. See the documentation for {@link RouteConfig} for details. */
@@ -138,7 +154,9 @@ export type RouterConfig<T extends string = string> = {
 };
 
 /**
- * This is the router that is created by {@link "index".createRouter}.
+ * Router created by {@link "index".createRouter}.
+ *
+ * @template T See {@link RouteConfig} for details.
  */
 export type Router<T extends string = string> = {
   /** The initial route as parsed from the location bar. */
@@ -153,6 +171,6 @@ export type Router<T extends string = string> = {
   /** Function to start the router. */
   start: Start<T>;
 
-  /** Function that synchronizes the location bar with the* state route. */
+  /** Function that synchronizes the location bar with the application state route. */
   syncLocationBar: SyncLocationBar<T>;
 };
